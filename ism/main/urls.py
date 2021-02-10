ac5e582ac588109553a488e59b92e72a695b8a59
Dookie <andkir0408@gmail.com>
2021-02-10 20:47:55 +0500

    commit #2

--- ism/main/urls.py
+++ ism/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='doc'),
+    path('', views.index, name='main'),
     path('contact', views.contact, name='contacts')
 
 ]
